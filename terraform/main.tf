9da0e45b35e4e22f9d9fd20aa71f87ae91eaf241
abhiram11 <abhiramsatpute@gmail.com>
2022-11-04 18:00:12 +0530

    added output ip as sensitive for ssh expose in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "abhiserver" {
   ami                    = "ami-08c40ec9ead489470"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer_key.key_name
@@ -78,4 +78,9 @@ resource "aws_security_group" "secgroup" {
 resource "aws_key_pair" "deployer_key" {
   key_name   = var.key_name
   public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value     = aws_instance.abhiserver.public_ip
+  sensitive = true
 }
